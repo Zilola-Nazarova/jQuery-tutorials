fbddbdaee16f74277ed1ccc402a81088f28175ee
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-04-04 23:22:35 +0500

    Practice Fading Animation Example lesson

--- TheNetNinja/scripts/script.js
+++ TheNetNinja/scripts/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  alert("jquery loaded");
+  const allQuotes = $("blockquote");
+  let currentQuote = 0;
+
+  function changeQuote() {
+    $(allQuotes[currentQuote]).fadeOut(200, function() {
+      if(currentQuote == allQuotes.length - 1) {
+        currentQuote = 0;
+      } else {
+        currentQuote++;
+      }
+      $(allQuotes[currentQuote]).fadeIn(200);
+    });
+  }
+
+  const quoteTimer = setInterval(changeQuote, 3000);
 });
